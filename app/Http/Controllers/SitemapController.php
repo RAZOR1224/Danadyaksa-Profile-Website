59603d0c12480ab950e253c190a084f76ff790e7
Khalid <118032316+RAZOR1224@users.noreply.github.com>
2025-08-07 23:21:11 +0700

    a

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,47 +2,33 @@
 
 namespace App\Http\Controllers;
 
-// Import all the necessary classes
-use Illuminate\Http\Request;
-use App\Models\Article;
-use App\Models\Team;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
-    /**
-     * Generate the sitemap.
-     */
     public function generate()
     {
         $sitemap = Sitemap::create();
 
-        // Define your static pages
         $staticPages = ['/', '/about', '/services', '/team', '/articles', '/contact'];
 
-        // Add each static page with its language alternatives
         foreach ($staticPages as $page) {
             $sitemap->add(
                 Url::create('/en' . $page)
-                    ->addAlternate('/id' . $page, 'id') // Assumes you also have an 'id' locale
+                    ->addAlternate('/id' . $page, 'id')
                     ->setPriority(0.8)
                     ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
             );
         }
-        
-        // This part requires your Article and Team models to be set up for multilingual routes.
-        // For now, focus on getting the static pages correct.
-        // You can add logic here later to add your dynamic multilingual URLs.
-        // For example:
-        //
-        // foreach (Article::all() as $article) {
-        //     $sitemap->add(
-        //         Url::create('/en/articles/' . $article->slug)
-        //             ->addAlternate('/id/articles/' . $article->slug, 'id')
-        //     );
-        // }
 
-        return $sitemap;
+        // Manually get the XML content as a string using the render() method.
+        $xmlContent = $sitemap->render();
+
+        // Manually create a new Response with the XML content and the correct header.
+        // This is the most direct and foolproof way.
+        return response($xmlContent, 200, [
+            'Content-Type' => 'application/xml'
+        ]);
     }
 }
